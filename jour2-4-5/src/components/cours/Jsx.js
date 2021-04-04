44c47db4acfde21f22884417aeccc3c7e6d1becd
alexandre-aco-code <alexandre.connanglecode@gmail.com>
2021-04-04 19:16:05 +0200

    last changes jour5

--- jour2-4-5/src/components/cours/Jsx.js
+++ jour2-4-5/src/components/cours/Jsx.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 import FormInput from "../forms/FormInput";
 
 class Jsx extends Component {
@@ -7,56 +8,82 @@ class Jsx extends Component {
         this.state = {
             isVisible: true,
             contact: {
-                nom: '',
-                phone: '',
-                email: ''
-            }
-        }
+                nom: "",
+                phone: "",
+                email: "",
+            },
+        };
 
         this.submit = this.submit.bind(this);
+        this.updateStateContact.bind(this);
     }
 
+    updateStateContact = (e) => {
+        const { contact } = this.state;
 
-    submit(e) {
+        contact[e.target.name] = e.target.value;
+
+        this.setState({ contact });
+    };
+
+    async submit(e) {
         e.preventDefault();
-        // récupérer les input du formulaire
-        const inputs = e.target.querySelectorAll('input');
-        // boucler autour des inputs
-        // créer un objet contact
-        const contact = {};
-        inputs.forEach(input => {
-            // assigné les valeurs des inputs a leur name dans l'objet contact
-            contact[input.name] = input.value;
-        });
 
+        const { contact } = this.state;
+        // TODO: vérification de la validité des champs
+
+        // On post en BDD
+        const res = await axios.post(
+            "https://jsonplaceholder.typicode.com/users/",
+            contact
+        );
+
+        if (res.status === 201) {
+            // Si j'ai un state valide, j'ajoute le contact.
+            this.setState({ contact: res.data });
+        }
+
+        // name = '';
+        // phone = '';
+        //
+        // contact = {
+        //     name
+        //     phone
+        // }
         // et les mettre dans le state. (Syntaxe moderne)
-        this.setState({contact});
     }
-
 
     render() {
         return (
             <form onSubmit={this.submit}>
                 <FormInput
+                    onChange={this.updateStateContact}
                     type="nom"
                     label="Votre Nom"
                     id="nom"
                     name="nom"
-                    placeholder="Votre Nom"/>
+                    placeholder="Votre Nom"
+                />
                 <FormInput
+                    onChange={this.updateStateContact}
                     type="email"
                     label="Votre Email"
                     id="email"
                     name="email"
-                    placeholder="Votre Email"/>
+                    placeholder="Votre Email"
+                />
                 <FormInput
+                    onChange={this.updateStateContact}
                     type="tel"
                     label="Votre Téléphone"
                     id="phone"
                     name="phone"
-                    placeholder="Votre Téléphone"/>
+                    placeholder="Votre Téléphone"
+                />
 
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
             </form>
         );
     }
